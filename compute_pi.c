ed3b7c34a5fe1a3e006dfbee115811dbd94270e4
rf5940709 <rf5_g3129078@yahoo.com.tw>
2015-10-29 00:02:12 +0800

    Implement Euler version
    (pi^2)/6 = 1/(1^2) + 1/(2^2) + 1/(3^2) + ...

--- compute_pi.c
+++ compute_pi.c
@@ -51,6 +51,13 @@ double compute_pi_Gregory(size_t dt)
     }
     return pi * 4.0;
 }
+double compute_pi_Euler(size_t dt)
+{
+    double pi = 0;
+    for (size_t i = 1; i < dt; i++)
+        pi += (1/((double)i*(double)i));
+    return sqrt(pi*6);
+}
 int main(int argc, char *argv[])
 {
     size_t n = atoi(argv[1]);
@@ -77,6 +84,13 @@ int main(int argc, char *argv[])
     time = (double)(end - begin) / CLOCKS_PER_SEC;
     printf("compult_pi loop : %zu MB, vaile : %lf \n", n, value);
     printf("execution time of compute_pi_Gregory()       : %lf sec\n", time);
+
+    begin = clock();
+    value = compute_pi_Euler(n * 1024*1024);
+    end = clock();
+    time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("compult_pi loop : %zu MB, vaile : %lf \n", n, value);
+    printf("execution time of compute_pi_Euler()         : %lf sec\n", time);
 
     return 0;
 }
